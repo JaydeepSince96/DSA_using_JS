3a6bdefdd9a6ef11fb6f1aa1a12fb5bbe6058543
Jaydeep <jaydeepiitd95@gmail.com>
2023-05-03 10:14:05 +0530

    Traversing in an Array using Js now added

---
+++ Basic_arr_op/traversing.js
@@ -0,0 +1,10 @@
+// Traversing general meaning is fetch elements one by one inside an Arr;
+// We can traverse elements in arr by using simle for loop;
+
+function traversing() {
+  const arr = [2, 3, 8, 7, 45, 6];
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+traversing();
